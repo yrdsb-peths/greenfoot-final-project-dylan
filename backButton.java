03e4804527ef718e90126d34d950d9787a1818da
Dylan-Ta <101138961+Dylan-Ta@users.noreply.github.com>
2022-06-14 09:53:24 -0400

    BackButton changed

--- backButton.java
+++ backButton.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author Dylan Ta 
  * @version June, 2022
  */
-public class backButton extends Actor
+public class BackButton extends Actor
 {
     //Return to TitleWorld
     public void act()
